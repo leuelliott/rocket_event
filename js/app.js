4f5278b20e37135c989653443ead8dffa179b9ba
Deelesi Suanu <deelesisuanu@gmail.com>
2020-12-19 04:01:52 +0100

    chore(): Seperated Booking Page

--- js/app.js
+++ js/app.js
@@ -45,36 +45,7 @@ function eventData() {
                 }
             }
             document.getElementById("tableData").innerHTML = output;
-        }
-    };
-    requestObject.send();
-}
-
-function eventBookingData() {
-    let requestObject = new XMLHttpRequest();
-    requestObject.open("GET", "https://projects.deelesisuanu.com/elliot-events/bookEventData");
-    requestObject.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            if(this.responseText == "[]" || this.responseText == "[][]"){
-                return;
-            }
-            const responseData = JSON.parse(this.responseText);
-            let output = "";
-            var i = 1;
-            for (const key in responseData) {
-                if (responseData.hasOwnProperty(key)) {
-                    const element = responseData[key];
-                    output += `<tr>
-                                        <td>${i++}</td>
-                                        <td>${element.fname}</td>
-                                        <td>${element.email}</td>
-                                        <td>${element.bookingCode}</td>
-                                        <td>${element.eventName}</td>
-                                        <td>${element.created}</td>
-                                    </tr>`;
-                }
-            }
-            document.getElementById("tableData2").innerHTML = output;
+            $("#eventSpinner").hide();
         }
     };
     requestObject.send();
@@ -98,20 +69,13 @@ function removeData(id, name) {
 
 $(document).ready(function () {
 
-    /* $('#example').DataTable({
-        paging: true
-    });
-    $('#example2').DataTable(); */
+    $('#example').DataTable();
     
-    // $('.mainUserTable').DataTable();
-    $('table.mainUserTable').DataTable();
-
     $("#eventDescription").summernote();
     $("#btnSpinner").hide();
 
     eventData();
-    eventBookingData();
-
+    
     function generateFormData(object) {
         let formData = new FormData();
         Object.keys(object).forEach((key) => formData.append(key, object[key]));
